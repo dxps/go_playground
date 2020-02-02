11bcb87b7f3d68a85aa6bf016b445b425d57c508
devisions <marius.ileana@gmail.com>
2020-02-02 19:00:38 +0200

    andon-go > templates #1

--- andon-go/webapp/src/main.go
+++ andon-go/webapp/src/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"devisions.org/andon-go/webapp/view"
+	"html/template"
 	"log"
 	"net/http"
-
-	"devisions.org/andon-go/webapp/view"
+	"os"
 )
 
 func main() {
+
+	// The " -" before the action ends tells the template to consume any white space
+	// that exists between the end of the curly brace and the next content.
+	content := `{{/* a comment */ -}}
+	this is a template!`
+
+	t := template.New("my first template")
+	t, err := t.Parse(content)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = t.Execute(os.Stdout, t)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	view.RegisterStaticHandlers()
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
