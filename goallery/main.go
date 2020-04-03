306a89056e71cfe620d082a091946743b4678bfc
devisions <marius.ileana@gmail.com>
2020-04-03 21:50:08 +0300

    goallery > some steps forward

--- goallery/main.go
+++ goallery/main.go
@@ -2,15 +2,45 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h3>Goallery begins!</h3>")
-}
+var homeTemplate *template.Template
 
 func main() {
-	http.HandleFunc("/", rootHandler)
-	http.ListenAndServe(":3000", nil)
+
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/contact", contactHandler)
+	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+	http.ListenAndServe(":3000", r)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Get in touch with us by writing an email at "+
+		"<a href=\"mailto:support@goallery.com\">"+
+		"support@goallery.com</a>.")
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h3>Requested Page was not found</h3>")
 }
