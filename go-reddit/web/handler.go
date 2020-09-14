4a1655c8e4d04843d56e961b736f5ff69d1d162b
devisions <marius.ileana@gmail.com>
2020-09-14 23:31:59 +0300

    go-reddit > applying the UI layouts

--- go-reddit/web/handler.go
+++ go-reddit/web/handler.go
@@ -23,46 +23,36 @@ func NewHandler(store goreddit.Store) *Handler {
 
 	h.Use(middleware.Logger)
 
+	h.Get("/", h.HomeHandler())
 	h.Route("/threads", func(r chi.Router) {
 		r.Get("/", h.GetThreadsHandler())
 		r.Get("/new", h.NewThreadHandler())
 		r.Post("/", h.SaveThreadHandler())
+		r.Get("/{id}", h.ShowThreadHandler())
 		r.Post("/{id}/delete", h.DeleteThreadHandler())
+		r.Get("/{id}/new", h.ShowCreatePostHandler())
+		r.Post("/{id}", h.SavePostHandler())
+		r.Get("/{threadID}/{postID}", h.ShowPostsHandler())
 	})
 
 	return h
 }
 
-const getThreadsTemplate = `
-<html>
-<head>
-<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-<meta content="utf-8" http-equiv="encoding">
-</head>
-<body>
-<h2>Threads</h2>
-<dl>
-{{range .Threads}}
-	<dt>{{.Title}}</dt>
-	<dd>{{.Description}}</dd>
-	<dd>
-		<form action="/threads/{{.ID}}/delete" method="POST">
-			<button type="submit">Delete</button>
-		</form>
-	</dd>
-{{end}}
-</dl>
-<a href="/threads/new">Create thread</a>
-</body>
-</html>
-`
+func (h *Handler) HomeHandler() http.HandlerFunc {
+	tmpl := template.Must(template.ParseFiles("web/templates/layout.html", "web/templates/home.html"))
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html charset=UTF-8")
+		_ = tmpl.Execute(w, nil)
+	}
+}
 
 func (h *Handler) GetThreadsHandler() http.HandlerFunc {
 	// init
 	type data struct {
 		Threads []goreddit.Thread
 	}
-	tmpl := template.Must(template.New("").Parse(getThreadsTemplate))
+	tmpl := template.Must(template.ParseFiles(
+		"web/templates/layout.html", "web/templates/threads.html"))
 	// handler logic
 	return func(w http.ResponseWriter, r *http.Request) {
 		ts, err := h.store.GetThreads()
@@ -75,25 +65,16 @@ func (h *Handler) GetThreadsHandler() http.HandlerFunc {
 	}
 }
 
-const createThreadTemplate = `
-<h2>New Thread</h2>
-<form action="/threads" method="POST">
-	<table>
-		<tr>
-			<td>Title</td>
-			<td><input type="text" name="title" /></td>
-		</tr>
-		<tr>
-			<td>Description</td>
-			<td><input type="text" name="description" /></td>
-		</tr>
-	</table>
-	<button type="submit">Create thread</button>
-</form>
-`
+func (h *Handler) ShowThreadHandler() http.HandlerFunc {
+	tmpl := template.Must(template.ParseFiles("web/templates/layout.html", "web/templates/thread.html"))
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html charset=UTF-8")
+		_ = tmpl.Execute(w, nil)
+	}
+}
 
 func (h *Handler) NewThreadHandler() http.HandlerFunc {
-	tmpl := template.Must(template.New("").Parse(createThreadTemplate))
+	tmpl := template.Must(template.ParseFiles("web/templates/layout.html", "web/templates/thread_create.html"))
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html charset=UTF-8")
 		_ = tmpl.Execute(w, nil)
@@ -126,6 +107,39 @@ func (h *Handler) DeleteThreadHandler() http.HandlerFunc {
 			return
 		}
 		if err := h.store.DeleteThread(id); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/threads", http.StatusFound)
+	}
+}
+
+func (h *Handler) ShowCreatePostHandler() http.HandlerFunc {
+	tmpl := template.Must(template.ParseFiles("web/templates/layout.html", "web/templates/post_create.html"))
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html charset=UTF-8")
+		_ = tmpl.Execute(w, nil)
+	}
+}
+
+func (h *Handler) ShowPostsHandler() http.HandlerFunc {
+	tmpl := template.Must(template.ParseFiles("web/templates/layout.html", "web/templates/posts.html"))
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html charset=UTF-8")
+		_ = tmpl.Execute(w, nil)
+	}
+}
+
+func (h *Handler) SavePostHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		title := r.FormValue("title")
+		description := r.FormValue("description")
+
+		if err := h.store.SaveThread(&goreddit.Thread{
+			ID:          uuid.New(),
+			Title:       title,
+			Description: description,
+		}); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
