d7610a652b73cf97787070b066d1b555c93cdf0f
devisions <marius.ileana@gmail.com>
2020-10-14 07:47:48 +0300

    go-practice > merge_names refactoring

--- go-practice/slices/merge_names/merge_names.go
+++ go-practice/slices/merge_names/merge_names.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-
 	// should print Ava, Emma, Olivia, Sophia
 	fmt.Println(uniqueNames(
 		[]string{"Ava", "Emma", "Olivia"},
@@ -11,23 +10,22 @@ func main() {
 }
 
 func uniqueNames(a, b []string) []string {
+	var r []string
+	r = collectUniqueValues(r, a...)
+	r = collectUniqueValues(r, b...)
+	return r
+}
 
-	var result []string
-	for _, s := range a {
-		if !contains(result, s) {
-			result = append(result, s)
+func collectUniqueValues(r []string, vals ...string) []string {
+	for _, val := range vals {
+		if !contains(r, val) {
+			r = append(r, val)
 		}
 	}
-	for _, s := range b {
-		if !contains(result, s) {
-			result = append(result, s)
-		}
-	}
-	return result
+	return r
 }
 
 func contains(result []string, item string) bool {
-
 	for _, val := range result {
 		if val == item {
 			return true
