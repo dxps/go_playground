9eff9eb6642ec349177aca797eab960f837eba78
dxps <marius.ileana@gmail.com>
2021-07-25 11:16:13 +0300

    feat(dist-apps-gin/recipes-api): Upd, Del, Search Recipe

--- dist-apps-gin/recipes-api/go.mod
+++ dist-apps-gin/recipes-api/go.mod
@@ -3,7 +3,7 @@ module github.com/dxps/go-playground/dist-apps-gin/recipes-api
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/gin-gonic/gin v1.7.2
 	github.com/go-playground/validator/v10 v10.7.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
--- dist-apps-gin/recipes-api/main.go
+++ dist-apps-gin/recipes-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -28,10 +29,16 @@ func main() {
 
 	router.POST("/recipes", newRecipeHandler)
 	router.GET("/recipes", listRecipesHandler)
+	router.PUT("/recipes/:id", updateRecipeHandler)
+	router.DELETE("/recipes/:id", deleteRecipeHandler)
+	router.GET("/recipes/search", searchRecipeHandler)
 
 	log.Print("Starting up ...")
 	_ = router.Run()
 }
+
+// --------
+// Handlers
 
 func newRecipeHandler(c *gin.Context) {
 	var recipe Recipe
@@ -47,4 +54,58 @@ func newRecipeHandler(c *gin.Context) {
 
 func listRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
+}
+
+func updateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	idx := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			idx = i
+		}
+	}
+	if idx == -1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Recipe not found"})
+		return
+	}
+	recipes[idx] = recipe
+	c.JSON(http.StatusOK, recipe)
+}
+
+func deleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	idx := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			idx = i
+		}
+	}
+	if idx == -1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Recipe not found"})
+		return
+	}
+	recipes = append(recipes[:idx], recipes[idx+1:]...)
+	c.JSON(http.StatusOK, gin.H{"message": "Recipe has been deleted"})
+}
+
+func searchRecipeHandler(c *gin.Context) {
+	t := c.Query("tag")
+	res := make([]Recipe, 0)
+	for i := 0; i < len(recipes); i++ {
+		found := false
+		for _, tt := range recipes[i].Tags {
+			if strings.EqualFold(tt, t) {
+				found = true
+			}
+		}
+		if found {
+			res = append(res, recipes[i])
+		}
+	}
+	c.JSON(http.StatusOK, res)
 }
