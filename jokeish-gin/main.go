40984a6f5966963b8962fd9aceaf395b23028e45
dxps <marius.ileana@gmail.com>
2022-02-07 10:34:45 +0200

    chore(jokeish-gin): security enhancement

--- jokeish-gin/main.go
+++ jokeish-gin/main.go
@@ -15,13 +15,13 @@ type Joke struct {
 }
 
 var jokes = []Joke{
-	Joke{1, 0, "Did you hear about the restaurant on the moon? Great food, no atmosphere."},
-	Joke{2, 0, "What do you call a fake noodle? An Impasta."},
-	Joke{3, 0, "How many apples grow on a tree? All of them."},
-	Joke{4, 0, "Want to hear a joke about paper? Nevermind it's tearable."},
-	Joke{5, 0, "I just watched a program about beavers. It was the best dam program I've ever seen."},
-	Joke{6, 0, "Why did the coffee file a police report? It got mugged."},
-	Joke{7, 0, "How does a penguin build it's house? Igloos it together."},
+	{1, 0, "Did you hear about the restaurant on the moon? Great food, no atmosphere."},
+	{2, 0, "What do you call a fake noodle? An Impasta."},
+	{3, 0, "How many apples grow on a tree? All of them."},
+	{4, 0, "Want to hear a joke about paper? Nevermind it's tearable."},
+	{5, 0, "I just watched a program about beavers. It was the best dam program I've ever seen."},
+	{6, 0, "Why did the coffee file a police report? It got mugged."},
+	{7, 0, "How does a penguin build it's house? Igloos it together."},
 }
 
 func main() {
@@ -40,7 +40,7 @@ func main() {
 		api.POST("/jokes/like/:jokeId", LikeJoke)
 	}
 
-	_ = router.Run(":3000")
+	_ = router.Run(":8090")
 
 }
 
