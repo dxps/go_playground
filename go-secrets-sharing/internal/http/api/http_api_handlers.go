a50075052e3c72a23929235900244f88acdccf4a
dxps <marius.ileana@gmail.com>
2022-07-03 12:38:20 +0300

    refact(go-secrets-sharing): using responders

--- go-secrets-sharing/internal/http/api/http_api_handlers.go
+++ go-secrets-sharing/internal/http/api/http_api_handlers.go
@@ -2,88 +2,75 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"strings"
 
 	"github.com/dxps/go_playground/go-secrets-sharing/internal/apperrs"
-	log "github.com/sirupsen/logrus"
 )
 
 func (a *HttpApi) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusBadRequest)
+		a.respondError(w, fmt.Sprintf("Only GET method can be used."), http.StatusBadRequest)
+		return
 	}
 	// Blindly respond OK, for now.
-	// Ignore the HTTP method used or the current app state.
 	w.WriteHeader(http.StatusOK)
 }
 
 func (a *HttpApi) addSecretHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusBadRequest)
+		a.respondError(w, fmt.Sprintf("Only POST method can be used."), http.StatusBadRequest)
+		return
 	}
 
 	rBody, err := io.ReadAll(r.Body)
 	if err != nil {
-		log.Errorf("Request body read error: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
+		a.respondError(w, fmt.Sprintf("Cannot decode request body: %v", err))
+		return
 	}
 
 	var input AddSecretInput
 	if err := json.Unmarshal(rBody, &input); err != nil {
-		log.Errorf("Json unmarshall of input error: %v", err)
-		w.WriteHeader(http.StatusBadRequest)
+		a.respondError(w, fmt.Sprintf("Cannot decode request body: %v", err), http.StatusBadRequest)
+		return
 	}
 
 	hash, err := a.secrets.Store(input.PlainText)
 	if err != nil {
-		log.Errorf("[secretsHandler] Store error: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
+		a.respondError(w, fmt.Sprintf("Store error: %v", err))
 		return
 	}
 
 	respBody := NewAddSecretOutput(hash)
-	respData, err := json.Marshal(respBody)
-	if err != nil {
-		log.Errorf("[secretsHandler] json.Marshal error: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.Header().Add("Content-Type", "application/json")
-	_, _ = w.Write(respData)
+	a.respondJSON(w, respBody, http.StatusOK)
+	return
 }
 
 func (a *HttpApi) getSecretHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusBadRequest)
+		a.respondError(w, fmt.Sprintf("Only GET method can be used."), http.StatusBadRequest)
+		return
 	}
 
 	hash := strings.TrimPrefix(r.URL.Path, "/secrets/")
 	if len(strings.TrimSpace(hash)) == 0 {
-		w.WriteHeader(http.StatusBadRequest)
+		a.respondError(w, fmt.Sprintf("ID (the param, part of '/secrets/{ID}' URL path) is missing."), http.StatusBadRequest)
 		return
 	}
 	secret, err := a.secrets.Retrieve(hash)
 	if err != nil {
 		if err == apperrs.ErrEntryNotFound {
-			w.WriteHeader(http.StatusNotFound)
+			a.respondError(w, "Unknown ID.", http.StatusNotFound)
 		} else {
-			log.Errorf("[getSecretHandler] Got error: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
+			a.respondError(w, err.Error())
 		}
 		return
 	}
 	respBody := NewGetSecretOutput(secret)
-	respData, err := json.Marshal(respBody)
-	if err != nil {
-		log.Errorf("[getSecretHandler] json.Marshal error: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.Header().Add("Content-Type", "application/json")
-	_, _ = w.Write(respData)
+	a.respondJSON(w, respBody, http.StatusOK)
 }
---
+++ go-secrets-sharing/internal/http/api/responders.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func (a *HttpApi) respondJSON(w http.ResponseWriter, data any, status int, headers ...http.Header) {
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (a *HttpApi) respondError(w http.ResponseWriter, err string, status ...int) {
+
+	log.Error(err)
+	respStatus := http.StatusInternalServerError
+	if len(status) > 0 {
+		respStatus = status[0]
+	}
+	payload := struct {
+		Error string `json:"error"`
+	}{
+		Error: err,
+	}
+	a.respondJSON(w, payload, respStatus)
+}
