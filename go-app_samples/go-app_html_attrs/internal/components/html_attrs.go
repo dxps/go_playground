f7cedfcc0021e0610a6e591e24adc2150c273fe9
dxps <marius.ileana@gmail.com>
2025-04-02 10:58:52 +0300

    feat(go-app_html_attrs): using datasets

--- go-app_samples/go-app_html_attrs/internal/components/html_attrs.go
+++ go-app_samples/go-app_html_attrs/internal/components/html_attrs.go
@@ -21,37 +21,50 @@ func (d *HTMLAttrs) Render() app.UI {
 			app.Div().
 				Attr("id", "div-1-id").
 				Attr("title", "div-1-title").
-				Attr("data-div1", "div-1-some-data").
+				Attr("data-div1-1", "div-1-some-data-1").
+				DataSets(map[string]any{
+					"div1-1": "div-1-1 data",
+					"div1-2": "div-1-2 data",
+				}).
 				Text("Div 1").
 				OnClick(func(ctx app.Context, e app.Event) {
 					slog.Info("On Div 1",
 						"id", ctx.JSSrc().Get("id").String(),
 						"title", ctx.JSSrc().Get("title").String(),
-						"data-div1", ctx.JSSrc().Get("data-div1").String())
+						"data div1-1", ctx.JSSrc().Get("dataset").Get("div1-1").String(),
+						"data div1-2", ctx.JSSrc().Get("dataset").Get("div1-2").String())
 				}).
 				Class("bg-gray-100 rounded-md px-4 py-1 my-1 hover:bg-gray-200 hover:cursor-pointer"),
 			app.P().
 				Attr("id", "p-1-id").
 				Attr("title", "p-1-title").
-				Attr("data-p1", "p-1-some-data").
+				DataSets(map[string]any{
+					"p1-1": "p-1-1 data",
+					"p1-2": "p-1-2 data",
+				}).
 				Text("Paragraph 1").
 				OnClick(func(ctx app.Context, e app.Event) {
 					slog.Info("On Paragraph 1",
 						"id", ctx.JSSrc().Get("id").String(),
 						"title", ctx.JSSrc().Get("title").String(),
-						"data-p1", ctx.JSSrc().Get("data-p1").String())
+						"data p1-1", ctx.JSSrc().Get("dataset").Get("p1-1").String(),
+						"data p1-2", ctx.JSSrc().Get("dataset").Get("p1-2").String())
 				}).
 				Class("bg-gray-100 rounded-md px-4 py-1 my-1 hover:bg-gray-200 hover:cursor-pointer"),
 			app.Span().
 				Attr("id", "span-1-id").
 				Attr("title", "span-1-title").
-				Attr("data-span1", "span-1-some-data").
+				DataSets(map[string]any{
+					"span1-1": "span-1-1 data",
+					"span1-2": "span-1-2 data",
+				}).
 				Text("Span 1").
 				OnClick(func(ctx app.Context, e app.Event) {
 					slog.Info("On Span 1",
 						"id", ctx.JSSrc().Get("id").String(),
 						"title", ctx.JSSrc().Get("title").String(),
-						"data-span1", ctx.JSSrc().Get("data-span1").String())
+						"data span1-1", ctx.JSSrc().Get("dataset").Get("span1-1").String(),
+						"data span1-2", ctx.JSSrc().Get("dataset").Get("span1-2").String())
 				}).
 				Class("bg-gray-100 rounded-md px-4 py-1 my-1 hover:bg-gray-200 hover:cursor-pointer"),
 		)
